0433ca37d9625d982cdae92b82ad27d8254dc03d
Putta Khunchalee <ultimaweapon@outlook.com>
2017-10-02 18:33:37 +0700

    Add logic to update battery status

--- src/ui/batterystatus.cpp
+++ src/ui/batterystatus.cpp
@@ -1,7 +1,32 @@
 #include "batterystatus.h"
 
+#include <QFile>
+#include <QTextStream>
+
 BatteryStatus::BatteryStatus(QWidget *parent) :
     QProgressBar(parent)
 {
+    // Setup UI.
     setFixedWidth(100);
+    setMaximum(100);
+    setMinimum(0);
+    setValue(0);
+
+    // Get current status.
+    updateBatteryStatus();
+
+    // Polling.
+    connect(&poller, SIGNAL(timeout()), SLOT(updateBatteryStatus()));
+    poller.start(5000);
+}
+
+void BatteryStatus::updateBatteryStatus()
+{
+    QFile file("/sys/class/power_supply/BAT0/capacity");
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        return;
+    }
+
+    QTextStream stream(&file);
+    setValue(stream.readLine().toInt());
 }
--- src/ui/batterystatus.h
+++ src/ui/batterystatus.h
@@ -2,13 +2,23 @@
 #define BATTERYSTATUS_H
 
 #include <QProgressBar>
+#include <QTimer>
 
 class BatteryStatus : public QProgressBar
 {
     Q_OBJECT
 
 public:
-    BatteryStatus(QWidget *parent = Q_NULLPTR);
+    explicit BatteryStatus(QWidget *parent = nullptr);
+
+public slots:
+    void updateBatteryStatus();
+
+private:
+    QTimer poller;
+
+private:
+    Q_DISABLE_COPY(BatteryStatus)
 };
 
 #endif // BATTERYSTATUS_H
